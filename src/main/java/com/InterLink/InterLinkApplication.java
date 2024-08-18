67a74677d339e6fcf2c67e26d1e398ed4cddf842
Sarthak Gudwani <gudwanisarthak2000@gmail.com>
2024-08-18 22:31:01 +0530

    Initialized Springboot project

---
+++ src/main/java/com/InterLink/InterLinkApplication.java
@@ -0,0 +1,13 @@
+package com.InterLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InterLink/InterLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InterLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
